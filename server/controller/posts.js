81a88acf3767f2b1188c32db3abb0fd93e28685d
Saqib <saqi_mm@hotmail.com>
2021-05-16 01:07:46 +0500

    Promiserejection bug fix

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -4,8 +4,6 @@ export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
 
-    console.log(postMessages);
-
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -19,7 +17,7 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save();
-    res.json(200).json(newPost);
+    res.status(200).json(newPost);
   } catch (error) {
     res.json(409).json({ message: error.message });
   }
